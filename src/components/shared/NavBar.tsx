31b7c3c8153e4527ffe304033d8751562d498270
Nick Jensen <njensen@me.com>
2024-11-06 17:16:04 -0500

    Meeting Manifesto

--- src/components/shared/NavBar.tsx
+++ src/components/shared/NavBar.tsx
@@ -34,7 +34,7 @@ import { usePathname } from "next/navigation";
 //     "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
 // };
 const navigationItems = [
-  { name: "Manifesto", href: "/", current: true },
+  { name: "Meeting Manifesto", href: "/", current: true },
   { name: "Personal Cost Calculator", href: "/personal-toll", current: false },
   { name: "Meeting Cost Calculator", href: "/meeting-minutes", current: false },
   // { name: "Calendar", href: "#", current: false },
